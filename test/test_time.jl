44a6b6208ad84f31bc92a63d3eb3d51ce5fc7d92
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-10-15 23:12:55 -0300

    Initial commit

--- test/test_time.jl
+++ test/test_time.jl
@@ -4,16 +4,16 @@ using UnitConverter
 using Test
 
 @testset "Time" begin
-    @test get_conversion_factor("s", "minute") ≈ 1 / 60.0
-    @test get_conversion_factor("minute", "s") ≈ 60.0
+    @test convert_unit("s", "minute") ≈ 1 / 60.0
+    @test convert_unit("minute", "s") ≈ 60.0
 
-    @test get_conversion_factor("minute", "hour") ≈ 1 / 60.0
-    @test get_conversion_factor("hour", "minute") ≈ 60.0
+    @test convert_unit("minute", "hour") ≈ 1 / 60.0
+    @test convert_unit("hour", "minute") ≈ 60.0
 
-    @test get_conversion_factor("hour", "day") ≈ 1 / 24.0
-    @test get_conversion_factor("day", "hour") ≈ 24.0
+    @test convert_unit("hour", "day") ≈ 1 / 24.0
+    @test convert_unit("day", "hour") ≈ 24.0
 
-    @test get_conversion_factor("day", "week") ≈ 1 / 7.0
-    @test get_conversion_factor("week", "day") ≈ 7.0
+    @test convert_unit("day", "week") ≈ 1 / 7.0
+    @test convert_unit("week", "day") ≈ 7.0
 end
 end
