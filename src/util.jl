cc7e0c0466479c20fad8dc721bf43245d4fe5e8c
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-12-04 22:26:04 -0300

    Initial commit

--- src/util.jl
+++ src/util.jl
@@ -1,4 +1,4 @@
 function get_factor()
-    a = run(`units.exe -1 --compact "km/h" "m/s"`)
+    a = run(`units.exe -1 --compact "km/hour" "m/s"`)
     @show a
 end
--- test/util.jl
+++ test/util.jl
@@ -1,7 +1,7 @@
 function test_convert_unit(from_unit::String, to_unit::String, factor::Number)
     @testset "$from_unit and $to_unit" begin
-        @test convert_unit(from_unit, to_unit) ≈ factor
-        @test convert_unit(to_unit, from_unit) ≈ 1 / factor
+        @test convert_unit(from_unit, to_unit) ≈ factor rtol = 1e-4
+        @test convert_unit(to_unit, from_unit) ≈ 1 / factor rtol = 1e-4
     end
     return nothing
 end
